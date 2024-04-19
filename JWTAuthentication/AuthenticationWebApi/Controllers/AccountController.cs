7e7cdaaa4bee75e29c89ad735816d90de024a174
Phùng Hoài Thương <113285922+thuongphung46@users.noreply.github.com>
2024-04-19 08:14:17 +0700

    add api gateway

---
+++ JWTAuthentication/AuthenticationWebApi/Controllers/AccountController.cs
@@ -0,0 +1,27 @@
+﻿using JWTAuthentication;
+using JWTAuthentication.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthenticationWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly JwtTokenHandler _jwtTokenHandler;
+
+        public AccountController(JwtTokenHandler jwtTokenHandler)
+        {
+            _jwtTokenHandler = jwtTokenHandler;
+        }
+
+        [HttpPost]
+        public ActionResult<AuthenticationResponse?> Authenticate([FromBody] AuthenticationRequest request)
+        {
+            var authenticationResponse = _jwtTokenHandler.GenerateToken(request);
+            if (authenticationResponse == null)return Unauthorized();
+            return authenticationResponse;
+        }
+    }
+}
